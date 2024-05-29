3126645dd8db28af6a1171bb17f972a0b1cd0f7c
elyesszobli <elyesszobli@gmail.com>
2024-05-29 22:29:53 +0200

    terminer la fonction de trouver les mails

--- Scraping.py
+++ Scraping.py
@@ -1,7 +1,16 @@
 from bs4 import BeautifulSoup
 
-
-
+def find_html_page(page,found_emails):
+    find_emails(page,found_emails)
+    for link in page.find_all('a'):
+        if not link['href'].endswith('html'):
+            continue
+        if 'index.html' in link['href']:
+            continue
+        else:
+            page_to_open = BeautifulSoup(open(link['href'],'r',encoding='utf-8'),'html.parser')
+            find_html_page(page_to_open,found_emails)
+    return found_emails
 
 
 def find_emails(page,found_emails):
@@ -10,3 +19,14 @@ def find_emails(page,found_emails):
             found_email = email['href'].split(':')[-1]
             if found_email not in found_emails:
                 found_emails.append(found_email)
+                
+def main():
+    with open('index.html','r',encoding='utf-8') as html_file:
+        html_content = html_file.read()
+        soup = BeautifulSoup(html_content,'html.parser')
+        unique_emails = []
+        found = find_html_page(soup,unique_emails)
+        print(found)
+        
+if __name__ == '__main__':
+    main()
